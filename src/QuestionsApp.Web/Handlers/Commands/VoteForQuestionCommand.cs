1afc6fd215ec4af7a11613a674923b2ee8ea59c5
OlympeDeGouges <128899203+OlympeDeGouges@users.noreply.github.com>
2024-03-07 13:33:41 +0100

    VoteForQuestionCommand_and_Program

--- src/QuestionsApp.Web/Handlers/Commands/VoteForQuestionCommand.cs
+++ src/QuestionsApp.Web/Handlers/Commands/VoteForQuestionCommand.cs
@@ -1,6 +1,15 @@
 namespace QuestionsApp.Web.Handlers.Commands;
+using MediatR;
 
-public class VoteForQuestionCommand
+public class VoteForQuestionRequest : IRequest<IResult>
 {
-    
+    public int QuestionId { get; set; }
 }
+public class VoteForQuestionCommand : IRequestHandler<VoteForQuestionRequest, IResult>
+{
+    public Task<IResult> Handle(VoteForQuestionRequest request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
--- src/QuestionsApp.Web/Program.cs
+++ src/QuestionsApp.Web/Program.cs
@@ -1,9 +1,16 @@
+using MediatR;
+using QuestionsApp.Web.Handlers.Commands;
+using QuestionsApp.Web.Handlers.Queries;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
+// Register MediatR
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
 
 var app = builder.Build();
 
@@ -15,5 +22,18 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Queries
+app.MapGet("api/queries/questions", async (IMediator mediator) 
+    => await mediator.Send(new GetQuestionsRequest()));
+
+// Commands
+app.MapPost("api/commands/questions/", async (IMediator mediator, string content) 
+    => await mediator.Send(new AskQuestionRequest { Content = content }));
+
+app.MapPost("api/commands/questions/{id:int}/vote", async (IMediator mediator, int id) 
+    => await mediator.Send(new VoteForQuestionRequest { QuestionId = id }));
+
+app.Run();
 
 app.Run();
